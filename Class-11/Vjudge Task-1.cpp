3868845862b83a71265f698f5ac6e7feff92fd37
Muhammad Shahriar <37985651+MuhammadShahriar@users.noreply.github.com>
2023-05-09 23:07:13 +0600

    Vjudge Task-1.cpp

--- Class-11/Vjudge Task-1.cpp
+++ Class-11/Vjudge Task-1.cpp
@@ -1,11 +1,11 @@
 ///   ***   ---   ||         In the name of ALLAH        |||   ---   ***   ///
- 
- 
- 
+
+
+
 #include<bits/stdc++.h>
 using namespace std;
- 
- 
+
+
 typedef long long ll;
 typedef vector<int> vi;
 typedef vector<ll> vl;
@@ -17,7 +17,7 @@ typedef pair<ll, ll> pll;
 typedef vector<pii> vii;
 typedef vector<pll> vll;
 typedef double dl;
- 
+
 #define endl '\n'
 #define PB push_back
 #define F first
@@ -25,65 +25,71 @@ typedef double dl;
 #define all(a) (a).begin(),(a).end()
 #define rall(a) (a).rbegin(),(a).rend()
 #define sz(x) (int)x.size()
- 
+
 const double PI = acos(-1);
 const double eps = 1e-9;
 const int inf = 2000000000;
 const ll infLL = 9000000000000000000;
 #define MOD 1000000007
- 
+
 #define mem(a,b) memset(a, b, sizeof(a) )
 #define sqr(a) ((a) * (a))
- 
+
 #define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 #define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
 #define file() freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
- 
+
 #define dbg(args...) do {cerr << #args << " : "; faltu(args); } while(0)
 void faltu () {            cerr << endl;}
 template < typename T, typename ... hello>void faltu( T arg, const hello &... rest) {cerr << arg << ' ';faltu(rest...);}
- 
+
 ll gcd ( ll a, ll b ) { return __gcd ( a, b ); }
 ll lcm ( ll a, ll b ) { return a * ( b / gcd ( a, b ) ); }
- 
-const int mx = 1e5+123;
-int a[mx];
- 
+
 int main()
 {
     optimize();
- 
-    int n, q;
-    cin >> n >> q;
-    for ( int i = 0; i < n; i++ ) cin >> a[i];
- 
- 
-    while ( q-- ) {
-        int target;
-        cin >> target;
-        int l = 0, r = n-1, ans = n;
- 
-        while ( l <= r ) {
-            int mid = ( l + r ) / 2;
-            if ( a[mid] == target) {
-                ans = min ( ans, mid );
-            }
- 
-            if ( a[mid] >= target ) {
-                r = mid-1;
-            }
-            else {
-                l = mid+1;
-            }
-        }
- 
-        if ( ans == n ) {
-            ans = -1;
-        }
- 
-        cout << ans << endl;
+
+    int n, k;
+    vector<pair<int, int>> v;
+
+    cin >> n >> k;
+
+    for ( int i = 0; i < n; i++ ) {
+        int score, penalty;
+        cin >> score >> penalty;
+        pair<int, int> p;
+
+        p.first = score;
+        p.second = -penalty;
+
+        v.push_back(p);
     }
- 
+
+    sort ( v.rbegin(), v.rend() ); ///O(nlog2(n))
+
+    int ans = 0;
+    for ( int i = 0; i < v.size(); i++ ) {
+        if ( v[k-1].first == v[i].first && v[k-1].second == v[i].second ) {
+            ans++;
+        }
+    }
+
+    cout << ans << endl;
+
     return 0;
 }
- 
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
